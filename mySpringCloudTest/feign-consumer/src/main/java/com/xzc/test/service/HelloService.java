70be075869785e8bd2235052247a039d35da00a1
xiongzhichao2017 <1046082892@qq.com>
2021-05-27 16:37:57 +0800

    add Feign mudule

---
+++ mySpringCloudTest/feign-consumer/src/main/java/com/xzc/test/service/HelloService.java
@@ -0,0 +1,11 @@
+package com.xzc.test.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FeignClient("hello-service")
+public interface HelloService {
+
+    @RequestMapping("/hello")
+    String hello();
+}
